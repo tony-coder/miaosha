405593450632e237bc2f30fbc939a6c99e722449
tony-coder <1606771552@qq.com>
2021-12-12 22:27:18 +0800

    增加redis操作类单元测试

---
+++ src/test/java/com/bfx/miaosha/redis/RedisServiceTest.java
@@ -0,0 +1,31 @@
+package com.bfx.miaosha.redis;
+
+import com.bfx.miaosha.domain.User;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisServiceTest {
+    @Autowired
+    private RedisService redisService;
+
+    @Test
+    public void get() {
+        User user = redisService.get(UserKey.getById(), "1", User.class);
+        Assert.assertNotNull(user);
+        System.out.print(user);
+    }
+
+    @Test
+    public void set() {
+        User user = User.builder().id(1).name("111").build();
+        redisService.set(UserKey.getById(), "1", user);  // UserKey:id1
+
+    }
+}
