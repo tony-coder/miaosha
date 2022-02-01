ace81b0209fdd90d364efc39105135b342591bbe
tony-coder <1606771552@qq.com>
2022-02-01 17:03:16 +0800

    修复mybatis映射配置读不到的问题

--- src/main/java/com/bfx/miaosha/MiaoshaApplication.java
+++ src/main/java/com/bfx/miaosha/MiaoshaApplication.java
@@ -1,9 +1,15 @@
 package com.bfx.miaosha;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1、mybatis.mapper-locations在SpringBoot配置文件中使用，作用是扫描Mapper接口对应的XML文件，如果全程使用@Mapper注解，可以不使用该配置。
+ * 2、@MapperScan会扫描Mapper接口类，并生成对应的实现类。
+ */
 @SpringBootApplication
+@MapperScan("com.bfx.miaosha.mapper")
 public class MiaoshaApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/bfx/miaosha/mapper/GoodsMapper.java
+++ src/main/java/com/bfx/miaosha/mapper/GoodsMapper.java
@@ -6,12 +6,13 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Mapper
+@Component
 public interface GoodsMapper {
-    @Select("select g.*,mg.stock_count, mg.start_date, mg.end_date,mg.miaosha_price from miaosha_goods mg left join goods g on mg.goods_id = g.id")
+//    @Select("select g.*,mg.stock_count, mg.start_date, mg.end_date,mg.miaosha_price from miaosha_goods mg left join goods g on mg.goods_id = g.id")
     public List<GoodsVo> listGoodsVo();
 
     @Select("select g.*,mg.stock_count, mg.start_date, mg.end_date,mg.miaosha_price from miaosha_goods mg left join goods g on mg.goods_id = g.id where g.id = #{goodsId}")
